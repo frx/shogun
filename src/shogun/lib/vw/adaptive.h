e717aa0683dc331e7c0d1efd4da14912988fd59d
Shashwat Lal Das <fraggerx@gmail.com>
2011-07-25 16:54:00 +0530

    Additions and corrections to adaptive train functions.

--- src/shogun/lib/vw/adaptive.h
+++ src/shogun/lib/vw/adaptive.h
@@ -1,9 +1,21 @@
 #ifndef _VW_ADAPTIVE_H__
 #define _VW_ADAPTIVE_H__
 
-#include <shogun/lib/vw/vw_train.h>
+#include <shogun/lib/vw/vw_machine.h>
 #include <shogun/lib/vw/vw_types.h>
 
 namespace shogun
 {
-	class 
+	class VwAdaptiveMachine: public VwMachine
+	{
+
+	public:
+		void train(VwExample* &ex, float update);
+
+		void perform_update(float* weights, VwFeature& page_feature,
+				    v_array<VwFeature> &offer_features, size_t mask,
+				    float update, float g, VwExample* ex, size_t& ctr);
+	};
+}
+
+#endif
--- src/shogun/lib/vw/vw_machine.h
+++ src/shogun/lib/vw/vw_machine.h
@@ -5,4 +5,31 @@
 
 namespace shogun
 {
-	class VwTrain
+	
+class VwMachine
+{
+public:
+	VwMachine()
+	{
+		reg = NULL;
+	}
+
+	virtual ~VwMachine()
+	{
+	}
+
+	virtual void train(VwExample* &ex, float update) = 0;
+	
+	virtual void perform_update(float* weights, VwFeature& page_feature,
+				    v_array<VwFeature> &offer_features, size_t mask,
+				    float update, float g, VwExample* ex, size_t& ctr) = 0;
+
+protected:
+
+	VwRegressor *reg;
+
+	VwEnvironment *env;
+};
+
+}
+#endif
